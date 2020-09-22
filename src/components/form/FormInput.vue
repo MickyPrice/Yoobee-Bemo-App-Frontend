40397e4d5d263753854b5fde11d3c9a3ba58c57b
Michael Price <realmprice@gmail.com>
2020-09-22 14:25:40 +1200

    Add FormInput component

---
+++ src/components/form/FormInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <input class="text__base--heavy" :type="type" :name="name" :placeholder="placeholder" aria-label="Search for friends"/>
+</template>
+
+<script>
+export default {
+  props: ["placeholder","name","type"]
+};
+</script>
+
+<style scoped lang="scss">
+@import "../../scss/_variables.scss";
+input {
+  width: 100%;
+  margin-top: $margin-20;
+
+  height: $height-btn;
+  background-color: $white-300;
+  border-radius: $corners-10;
+  border: 2px solid $white-300;
+  outline: none;
+  overflow: hidden;
+  padding: 20px;
+  &[type="text"] {
+    padding-left: $padding-20;
+  }
+  &:focus {
+    border: 2px solid $purple-100;
+  }
+}
+</style>
