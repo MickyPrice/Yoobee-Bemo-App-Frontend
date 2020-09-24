72c4acc3cf6370dc451b2a5d28634cd8b2c8e48b
T.H <Thomashendrikz@Gmail.com>
2020-09-24 21:59:20 +1200

    Add messages

--- src/components/chats/ChatPushCard.vue
+++ src/components/chats/ChatPushCard.vue
@@ -1,37 +1,30 @@
 <template>
   <div class="chat-push-card">
     <div class="chat-push-card__message-box">
-      <ul class="messages">
-        <li class="me">hell hell hell</li>
-        <li class="me">hell hell hell</li>
-        <li class="him">hell hell hell</li>
-        <li class="him">hell hell hell</li>
-        <li class="him">hell hell hell</li>
-        <li class="him">hell hell hell</li>
-
-        <li class="him">hell hell hell</li>
-
-        <li
-          class="me"
-        >hell hell hel hell hell hellhell hell hellhell hell hellhell hell hellhell hell hellhell hell hellhell hell hellhell hell hellhell hell helll</li>
-
-        <li class="me">hell hell hell</li>
-      </ul>
+      <Messages :newMessage="newMessage" />
     </div>
     <ChatBox v-on:newPost="postNewMessage($event)" />
   </div>
 </template>
 
 <script>
-import ChatBox from "./ChatBox";
+import ChatBox from "./ChatBox.vue";
+import Messages from "./Messages.vue";
 
 export default {
+  data() {
+    return {
+      newMessage: "",
+    };
+  },
   components: {
     ChatBox,
+    Messages,
   },
   methods: {
     postNewMessage: function (event) {
       console.log("Just got a new post saying....." + event);
+      this.newMessage = event;
     },
   },
 };
@@ -52,44 +45,6 @@ export default {
   &__message-box {
     height: 100%;
     margin: 0 5%;
-    overflow: scroll;
-    overflow-anchor: none;
-
-    .messages {
-      overflow-anchor: auto;
-
-      li {
-        display: inline-block;
-        clear: both;
-        padding: 20px;
-        border-radius: 30px;
-        margin-bottom: 2px;
-        font-family: Helvetica, Arial, sans-serif;
-      }
-      .me {
-        float: right;
-        background: #0084ff;
-        color: #fff;
-      }
-
-      .him {
-        background: #eee;
-        float: left;
-      }
-
-      .him + .me {
-        border-bottom-right-radius: 5px;
-      }
-
-      .me + .me {
-        border-top-right-radius: 5px;
-        border-bottom-right-radius: 5px;
-      }
-
-      .me:last-of-type {
-        border-bottom-right-radius: 30px;
-      }
-    }
   }
 }
 </style>
---
+++ src/components/chats/Messages.vue
@@ -0,0 +1,51 @@
+<template>
+  <ul class="messages">
+    <li class="me">hello</li>
+    <li class="me">sssss</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ["newMessage"],
+  watch: {
+    newMessage: function () {
+      console.log("new message in" + this.newMessage);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/scss/_variables";
+
+.messages {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  li {
+    display: inline-block;
+    clear: both;
+    font-size: 0.75em;
+    max-width: 70%;
+    padding: 10px 20px;
+    border-radius: 30px;
+    margin-bottom: 2px;
+  }
+
+  .me {
+    float: right;
+    background: $purple-500;
+    color: $white-100;
+  }
+
+  .me + .me {
+    border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px;
+  }
+
+  .me:last-of-type {
+    border-bottom-right-radius: 30px;
+  }
+}
+</style>
