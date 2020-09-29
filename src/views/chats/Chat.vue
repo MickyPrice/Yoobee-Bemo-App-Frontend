e690b262735de76f32f82e3cb81e842a53cc1c79
ogriffithjones <ogriffithjones@gmail.com>
2020-09-30 10:12:55 +1300

    Add chat function

--- src/views/chats/Chat.vue
+++ src/views/chats/Chat.vue
@@ -1,23 +1,13 @@
 <template>
-  <h1>Chat to {{ $route.params.channelId }}</h1>
+  <h1>{{$route.params.channelId}}</h1>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
 export default {
-  data: function() {
-      return {
-          channel: this.$route.params.channelId
-      }
-  },
-  methods: {
-      ...mapActions(["joinChannel", "leaveChannel"])
-  },
-  created() {
-      this.joinChannel(this.channel);
-  },
-  destroyed() {
-      this.leaveChannel(this.channel);
-  }
-};
+
+}
 </script>
+
+<style>
+
+</style>
