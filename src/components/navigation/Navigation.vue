810834b7c7e373143ec0d610ac78fcfdc5f9b0a9
Michael Price <realmprice@gmail.com>
2020-10-05 08:42:05 +1300

    Replace alerts with settings in navigation

--- src/components/navigation/Navigation.vue
+++ src/components/navigation/Navigation.vue
@@ -1,5 +1,8 @@
 <template>
-  <nav class="nav" :style=" { backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0), ${gradientColour} 60%)` }">
+  <nav
+    class="nav"
+    :style=" { backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0), ${gradientColour} 60%)` }"
+  >
     <router-link
       active-class="nav__link--active"
       :class="{ 'nav__link--light': !darkTheme }"
@@ -88,9 +91,9 @@
     </router-link>
     <router-link
       active-class="nav__link--active"
-      :class="{ 'nav__link--light': !darkTheme, 'nav__link--notification': alertsNotification }"
+      :class="{ 'nav__link--light': !darkTheme }"
       class="nav__link"
-      to="/alerts"
+      to="/settings"
     >
       <svg
         class="nav__svg"
@@ -103,7 +106,13 @@
           stroke-linecap="round"
           stroke-linejoin="round"
           stroke-width="2"
-          d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+          d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+        />
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
         />
       </svg>
     </router-link>
@@ -162,7 +171,7 @@ export default {
 
     // Large button
     &--large {
-        background-color: $purple-500;
+      background-color: $purple-500;
       // background-color: $black-300;
       color: $white-100;
       padding: 16px 20px;
@@ -180,14 +189,15 @@ export default {
     }
 
     &--notification::after {
-        content: '';
-        background-color: $red-300;
-        height: 8px; width: 8px;
-        display: block;
-        position: absolute;
-        top: 2px;
-        right: 2px;
-        border-radius: 100%;
+      content: "";
+      background-color: $red-300;
+      height: 8px;
+      width: 8px;
+      display: block;
+      position: absolute;
+      top: 2px;
+      right: 2px;
+      border-radius: 100%;
     }
   }
 }
