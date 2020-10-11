3ff99398f398e8d6a212284decebad798eb14a0f
Michael Price <realmprice@gmail.com>
2020-10-11 16:35:45 +1300

    Hide message text box from payment page for sake of MVP

--- src/components/payment/PayRequest.vue
+++ src/components/payment/PayRequest.vue
@@ -32,7 +32,8 @@
       tabindex="0"
       @keyup="paymentInput"
     ></div>
-    <FormInput type="text" name="message" placeholder="💬 Message..." />
+    <!-- Hidden for sake of MVP -->
+    <!-- <FormInput :v-if="false" type="text" name="message" placeholder="💬 Message..." /> -->
 
     <BtnFull
       v-if="mode == 'SEND'"
@@ -63,12 +64,12 @@
 
 <script>
 import BtnFull from "@/components/buttons/BtnFull";
-import FormInput from "@/components/form/FormInput";
+// import FormInput from "@/components/form/FormInput";
 
 export default {
   components: {
     BtnFull,
-    FormInput,
+    // FormInput,
   },
   data: function () {
     return { amount: 0.0 };
